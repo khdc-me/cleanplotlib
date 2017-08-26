ca2db3e20ed2bcca73a8cfe8a242a7638b1fce18
khdc-me <kevinharperg@gmail.com>
2017-08-26 07:29:21 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='cleanplotlib',
+      version='0.1',
+      description='Package to easily create clean (pretty/junk-free)'
+                  + 'matplotlib charts.',
+      url='https://github.com/khdc-me/cleanplotlib',
+      author='Kevin Harper',
+      author_email = 'kevinharperg@gmail.com',
+      license='MIT',
+      packages=['cleanplotlib'],
+      )
